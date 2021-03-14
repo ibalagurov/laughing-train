7710599c4d826dd27429180bac8c512c20888549
Igor Balagurov <i.balagurov@corp.vk.com>
2021-03-14 12:16:38 +0300

    Add tests

--- tests/test_gregorian_calendar.py
+++ tests/test_gregorian_calendar.py
@@ -1,9 +1,88 @@
+"""
+Tests for checking gregorian calendar date.
+
+Astronomical year contains 365,2425 days:
+365 for usual year and 366 for leap
+
+Leap years:
+0.2425 is 97 / 400 or 1/4 - 1/100 + 1/400
+
+It means:
+- each 4th year is leap, except 3 of 4 round dates
+- 2004, 2008, 2012 and etc are leap
+- 2000, 2400, 2800 and etc. is leap
+- 2100, 2200, 2300, 2500, 2600, 2700, 2900 and etc. are NOT leap
+- for 400 years 97 are leap
+"""
 import pytest
 
 from src import gregorian_calendar
 
 
-@pytest.mark.parametrize("string_date", ["01-01-1900"])
-def test_correct_date_format(string_date):
-    result = gregorian_calendar.correct(string=string_date)
-    assert result, f"Correct date '{string_date}' was recognized as incorrect"
+@pytest.mark.parametrize(
+    "year, month, day",
+    [
+        pytest.param(1, 1, 1, id="Minimum supported date"),
+        pytest.param(9999, 12, 31, id="Maximum supported date"),
+        pytest.param(4, 2, 29, id="First supported usual 4th leap year"),
+        pytest.param(2020, 2, 29, id="Usual 4th leap year"),
+        pytest.param(9996, 2, 29, id="Last supported usual 4th leap year"),
+        pytest.param(400, 2, 29, id="First supported round leap year"),
+        pytest.param(2000, 2, 29, id="Usual round leap year"),
+        pytest.param(9600, 2, 29, id="Last supported round leap year"),
+    ],
+)
+def test_correct_date_format(year, month, day):
+    """Check correct date"""
+    result = gregorian_calendar.is_correct_date(year=year, month=month, day=day)
+    assert result, f"Correct date '{day}-{month}-{year}'(day-month-year) is recognized as incorrect"
+
+
+def test_400_years_contain_97_leap_years():
+    """Check property of count leap years for 400 consecutive years"""
+    start_year = 2000
+    leap_years = [
+        year
+        for year in range(start_year, start_year + 400)
+        if gregorian_calendar.is_correct_date(year=year, month=2, day=29)
+    ]
+    actual_count = len(leap_years)
+    expected_count = 97
+    assert actual_count == expected_count, (
+        f"For 400 consecutive years '{expected_count}' should be leap. "
+        f"But actual count: '{actual_count}'. "
+        f"Years, recognized as leap:\n{leap_years}"
+    )
+
+
+@pytest.mark.parametrize(
+    "year, month, day",
+    [
+        # 29th february for not leap years
+        pytest.param(1, 2, 29, id="First supported usual year"),
+        pytest.param(2021, 2, 29, id="Usual year"),
+        pytest.param(9999, 2, 29, id="Last supported usual year"),
+        pytest.param(100, 2, 29, id="First supported round usual year"),
+        pytest.param(2100, 2, 29, id="Usual round year"),
+        pytest.param(9900, 2, 29, id="Last supported round usual year"),
+        # year format
+        pytest.param(0, 1, 31, id="Unsupported bottom boundary year"),
+        pytest.param(10_000, 1, 1, id="Unsupported top boundary year"),
+        pytest.param(-1, 1, 31, id="Negative year"),
+        # day format
+        pytest.param(1900, 1, 32, id="Nonexistent 32th day"),
+        pytest.param(1900, 4, 31, id="Nonexistent 31th day"),
+        pytest.param(1900, 1, 0, id="Nonexistent 0th day"),
+        pytest.param(1900, 1, -1, id="Negative day"),
+        # month format
+        pytest.param(1900, 0, 1, id="Nonexistent 0th day"),
+        pytest.param(1900, 13, 1, id="Nonexistent 13th month"),
+        pytest.param(1900, -1, 1, id="Negative month"),
+    ],
+)
+def test_incorrect_date_format(year, month, day):
+    """Check incorrect date"""
+    result = gregorian_calendar.is_correct_date(year=year, month=month, day=day)
+    assert (
+        not result
+    ), f"Incorrect date '{day}-{month}-{year}'(day-month-year) is recognized as correct"
